a73fc8d581ccb72908dfad6ec0bdf363e5760442
phong0210 <phong01062019@gmail.com>
2024-10-02 23:30:34 +0700

    tạo bảng order

--- BackEnd/src/main/java/com/example/demo/entity/Order.java
+++ BackEnd/src/main/java/com/example/demo/entity/Order.java
@@ -1,26 +1,53 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "`order`")
 public class Order {
+
     @Id
-
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_id", columnDefinition = "CHAR(36)")
     private String orderId;
+
+    @Column(name = "account_id", nullable = false)
     private String accountId;
-    private Date orderDate;
-    private Date shippedDate;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+
+    @Column(name = "shipped_date")
+    private LocalDateTime shippedDate;
+
+    @Column(name = "origin")
     private String origin;
+
+    @Column(name = "destination")
     private String destination;
+
+    @Column(name = "freight")
     private String freight;
+
+    @Column(name = "total_price")
     private float totalPrice;
+
+    @Column(name = "service_id")
     private String serviceId;
+
+    @Column(name = "document_id")
     private String documentId;
+
+    @Column(name = "status")
     private int status;
-
-
 }
