c447439055e91c3af86a7c231c461c62e18fa7c9
phong0210 <phong01062019@gmail.com>
2024-10-04 12:28:10 +0700

    thêm chức năng Order

--- BackEnd/src/main/java/com/example/demo/service/iml/OrderService.java
+++ BackEnd/src/main/java/com/example/demo/service/iml/OrderService.java
@@ -1,14 +1,17 @@
 package com.example.demo.service.iml;
 
 
+import com.example.demo.dto.request.AccountDTO;
 import com.example.demo.dto.request.OrderDTO;
 import com.example.demo.entity.Account;
+import com.example.demo.entity.IdGenerator;
 import com.example.demo.entity.Order;
 import com.example.demo.mapper.OrderMapper;
 import com.example.demo.repository.AccountRepository;
 import com.example.demo.repository.OrderRepository;
 import com.example.demo.exception.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,17 +20,23 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 
 public class OrderService {
-
+    @Autowired
     private final OrderRepository orderRepository;
+    @Autowired
     private final AccountRepository accountRepository;
+    @Autowired
     private final OrderMapper orderMapper;
 
     public OrderDTO createOrder(OrderDTO orderDTO) {
+        System.out.println("Received OrderDTO: " + orderDTO);
 
         Account account = accountRepository.findById(orderDTO.getAccountId())
                 .orElseThrow(() -> new ResourceNotFoundException("Account not found with id " + orderDTO.getAccountId()));
 
+        System.out.println("Account found: " + account);
+
         Order order = orderMapper.mapToOrder(orderDTO, account);
+        order.setOrderId(IdGenerator.generateId());
 
         if (orderDTO.getOrderDate() == null) {
             order.setOrderDate(LocalDateTime.now());
@@ -43,6 +52,7 @@ public class OrderService {
 
         return orderMapper.mapToOrderDTO(savedOrder);
     }
+
 
 
 }
