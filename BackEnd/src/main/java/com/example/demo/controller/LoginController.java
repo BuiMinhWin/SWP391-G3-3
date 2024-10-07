b7dd4376f27a60c631a9afe5d55116800aff3a41
Kiet0504 <kiettgse180500@fpt.edu.vn>
2024-10-08 00:12:59 +0700

    sửa

--- BackEnd/src/main/java/com/example/demo/controller/LoginController.java
+++ BackEnd/src/main/java/com/example/demo/controller/LoginController.java
@@ -4,7 +4,6 @@ import com.example.demo.dto.request.LoginDTO;
 import com.example.demo.Login.LoginMessage;
 import com.example.demo.service.iml.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -19,13 +18,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginMessage> login(@RequestBody LoginDTO loginDTO) {
-        LoginMessage loginMessage = accountService.loginUser(loginDTO);
-
-        if (loginMessage.getStatus()) {
-            return new ResponseEntity<>(loginMessage, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(loginMessage, HttpStatus.UNAUTHORIZED);
-        }
+        return accountService.loginUser(loginDTO);
     }
 
     @GetMapping("/google")
