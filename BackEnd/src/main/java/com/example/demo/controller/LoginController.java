dac47ef45b37e262722c9c1e5fe5e00de7468bc1
Kiet0504 <kiettgse180500@fpt.edu.vn>
2024-10-07 23:59:34 +0700

    thêm phương thức login GG trên swagger UI

--- BackEnd/src/main/java/com/example/demo/controller/LoginController.java
+++ BackEnd/src/main/java/com/example/demo/controller/LoginController.java
@@ -6,6 +6,8 @@ import com.example.demo.service.iml.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,5 +22,9 @@ public class LoginController {
         return accountService.loginUser(loginDTO);
     }
 
+    @GetMapping("/google")
+    public OAuth2User getUser(@AuthenticationPrincipal OAuth2User principal) {
+        return principal;
+    }
 
 }
