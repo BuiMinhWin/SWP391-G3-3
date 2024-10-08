a0c836b43ff2cf28781626bddfd89172eaa2bb67
Minh Thang <bmthang2004@gmail.com>
2024-10-08 23:52:16 +0700

    TestGoogle

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -10,17 +10,21 @@ export const createAccount = (account) => {
   return axios.post(REST_API_BASE_URL + '/register', account);
 };
 
+
 export const getAccount = (accountId) => {
   return axios.get(REST_API_BASE_URL + '/' + accountId);
 };
+
 
 export const updateAccount = (accountId, account) => {
   return axios.put(REST_API_BASE_URL + '/' + accountId, account);
 };
 
+
 export const deleteAccount = (accountId) => {
   return axios.delete(REST_API_BASE_URL + '/' + accountId);
 };
+
 
 export const loginAccount = (loginData) => {
   return axios.post(REST_API_BASE_URL + '/login', loginData, {
@@ -28,3 +32,17 @@ export const loginAccount = (loginData) => {
     withCredentials: true,
   });
 };
+
+
+export const googleLogin = (token) => {
+  return axios.post(
+    'http://koideliverysystem.id.vn:8080/api/accounts/google-login', 
+    { token },
+    {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,  
+    }
+  );
+};
+
+
