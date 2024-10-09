ba446ad57266cf957dc00bb0921a4d291f9d90d4
Kiet0504 <kiettgse180500@fpt.edu.vn>
2024-10-09 17:17:05 +0700

    sửa login GG

---
+++ BackEnd/src/main/java/com/example/demo/dto/request/GoogleLoginDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto.request;
+
+public class GoogleLoginDTO {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
