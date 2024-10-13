877bd7e1069cb35357069f8f488d535f8dadb462
Kiet0504 <kiettgse180500@fpt.edu.vn>
2024-10-13 16:27:46 +0700

    xóa bớt thư viện rác

--- BackEnd/src/main/java/com/example/demo/config/SpringSecurity.java
+++ BackEnd/src/main/java/com/example/demo/config/SpringSecurity.java
@@ -4,8 +4,6 @@ import com.example.demo.service.iml.AccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
