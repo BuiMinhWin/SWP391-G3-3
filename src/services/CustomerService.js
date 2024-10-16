e06278a33ffa3f888cc0972d8200b1bb243aa6ab
Nguyễn Anh Quân <syrexd42@gmail.com>
2024-10-16 22:05:58 +0700

    update file upload

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -13,23 +13,34 @@ export const createOrder = async (orderData) => {
     const response = await axios.post(`${REST_API_BASE_URL}/create`, orderData);
     return response.data;
   } catch (error) {
-    console.error("Error creating order:", error.response || error.message);
+    console.error("Error creating order:", error.message); // Log only the message
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("Request data:", error.request);
+    }
     throw error;
   }
 };
 
-export const createDocument = async (DocumentData) => {
+export const uploadDocument = async (file, orderId) => {
+  const formData = new FormData();
+  formData.append('document_file', file);
+  formData.append('orderId', orderId); // Attach orderId to the form data
+
   try {
-    const response = await axios.post(
-      `${REST_API_DOCUMENT_URL}/create`,
-      DocumentData
-    );
+    const response = await axios.post(`${REST_API_DOCUMENT_URL}/${orderId}`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
     return response.data;
   } catch (error) {
-    console.error("Error creating order:", error.response || error.message);
-    throw error;
+    console.error('File upload failed:', error);
+    throw new Error('Upload failed');
   }
 };
+
 export const createOrderDetail = async (orderDetailData) => {
   try {
     const response = await axios.post(
